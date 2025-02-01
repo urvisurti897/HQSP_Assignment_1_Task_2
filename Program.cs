fce1831fdb77fcdf333ec594926f1a9a45ec1482
urvisurti897 <urvisurti897@gmail.com>
2025-01-31 21:39:37 -0500

    Initializing the variables

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
 ﻿namespace HQSP_Assignment_1_Task_2
 {
+    //Task 2 - Total money spent on watching games
+
     internal class Program
     {
+
+        //Initializing the variables
+        static int PurplePrice = 50;
+        static int GreenPrice = 80;
+        static int BluePrice = 100;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
         }
     }
 }
